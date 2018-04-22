c91d0c2eb776919bdd2e7e097be64b99c6383c98
ALAN-PC\ALAN <aaron0481@gmail.com>
2018-04-22 20:09:18 +0800

    第二關到第三關，chess突然載入唔到

---
+++ Assets/Script/gameInstructionEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameInstructionEvent : MonoBehaviour
+{
+    public static gameInstructionEvent instance;
+
+    [SerializeField]
+    private GameObject gameInstructionEventPanel;
+
+    private void Awake()
+    {
+        MakeInstance();
+    }
+
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void gameInstructionEventGame()
+    {
+        Time.timeScale = 0f;
+        gameInstructionEventPanel.SetActive(true);
+    }
+
+    public void closeGameInstructionEventGame()
+    {
+        Time.timeScale = 1f;
+        gameInstructionEventPanel.SetActive(false);
+    }
+}
